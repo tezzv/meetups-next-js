4258e25a1872b76ca0bd24f82fb08a498f603855
tezzv <106775682+tezzv@users.noreply.github.com>
2023-05-15 10:06:01 +0530

    description updated

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,24 @@ const HomePage = (props) => {
   return (
     <>
       <Head>
+        {/* <!-- HTML Meta Tags --> */}
         <title>Meetups</title>
-        <meta name='description' content='Some popular meetups' />
-        <meta property="og:title" content="Meetups" />
-        <meta property="og:description" content="Some popular meetups" />
+        <meta name="description" content="Find the best places to meet up with friends and family while exploring popular tourist spots and historic monuments." />
+
+        {/* <!-- Facebook Meta Tags --> */}
         <meta property="og:url" content="https://next-js-demo-project1-git-master-tezzv.vercel.app/" />
-        <meta property="og:image" content='https://next-js-demo-project1-git-master-tezzv.vercel.app/images/meetups-preview.jpeg' />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Meetups" />
+        <meta property="og:description" content="Find the best places to meet up with friends and family while exploring popular tourist spots and historic monuments." />
+        <meta property="og:image" content="https://next-js-demo-project1-git-master-tezzv.vercel.app/images/meetups-preview.jpeg" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="next-js-demo-project1-git-master-tezzv.vercel.app" />
+        <meta property="twitter:url" content="https://next-js-demo-project1-git-master-tezzv.vercel.app/" />
+        <meta name="twitter:title" content="Meetups" />
+        <meta name="twitter:description" content="Find the best places to meet up with friends and family while exploring popular tourist spots and historic monuments." />
+        <meta name="twitter:image" content="https://next-js-demo-project1-git-master-tezzv.vercel.app/images/meetups-preview.jpeg" />
       </Head>
       <MeetupList meetups={props.meetups} />
 
